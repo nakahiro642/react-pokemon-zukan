fae891a036f024eb60e139807d066aeaf735b1e5
nakahiro642 <ktc25a32j0005@edu.kyoto-tech.ac.jp>
2025-10-11 09:44:30 +0900

    fix: update copyright year in Footer component to 2025

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-200 text-center p-4 mt-8">
-      <p>&copy; 2024 Reactポケモン図鑑</p>
+      <p>&copy; 2025 Reactポケモン図鑑</p>
     </footer>
   );
 };
